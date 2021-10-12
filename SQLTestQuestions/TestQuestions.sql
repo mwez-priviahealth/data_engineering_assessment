b21c9e45653bbd8c99b6a56b36df03992dffc7d3
jdterrie <jdterrie@outlook.com>
2021-10-11 23:49:05 -0400

    Sending SQL queries now.

--- SQLTestQuestions/TestQuestions.sql
+++ SQLTestQuestions/TestQuestions.sql
@@ -19,11 +19,10 @@ query or group of queries that return the patient name, and their most recent ri
 Any patients that dont have a risk level should also be included in the results. 
 
 **********************/
-
-
-
-
-
+SELECT Person.PersonName, Risk.RiskLevel, Risk.RiskDateTime
+FROM dbo.Person
+Left Join dbo.Risk ON Person.PersonID = Risk.PersonID
+ORDER BY PersonName, Risk.RiskDateTime DESC
 /**********************
 
 QUESTION 2
@@ -36,7 +35,8 @@ or be blank if no nickname exists.
 
 **********************/
 
-
+SELECT PersonName, SUBSTRING(PersonName, PATINDEX('%[(]%', PersonName) + 1, (PATINDEX('%[)]%', PersonName) - PATINDEX('%[(]%', PersonName) - 1)) AS Nickname FROM dbo.Person
+WHERE PATINDEX('%[)]%', PersonName) - PATINDEX('%[(]%', PersonName) != 0;
 
 /**********************
 
@@ -46,7 +46,10 @@ Write a query to return risk data for all patients, all payers
 and a moving average of risk for that patient and payer in dbo.Risk. 
 
 **********************/
-
-
-
-
+SELECT RiskScore, Person.PersonID, PersonName, AttributedPayer
+FROM dbo.Person
+Inner Join dbo.Risk ON Person.PersonID = Risk.PersonID
+SELECT DISTINCT Risk.PersonID, AttributedPayer, AVG(Risk.RiskScore) 
+OVER (PARTITION BY Risk.PersonID, AttributedPayer) AS MovingAverage
+FROM dbo.Risk
+Inner Join dbo.Person ON Person.PersonID = Risk.PersonID
