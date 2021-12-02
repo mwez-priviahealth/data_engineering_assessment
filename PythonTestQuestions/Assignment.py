7fd99a0f0c44d731dffd954a1be02ecf12de7199
yahuey <30640620+yahuey@users.noreply.github.com>
2021-12-01 19:03:58 -0700

    Decided to not use the pivots as it wasn't making much sense to me. I can understand it for different datasets but not for this one. So if that fails me, then okay. I just couldn't make sense of it, or make it look as clean as I can with other methods. I'd love to learn how it's done that specific way if I get the chance.

--- PythonTestQuestions/Assignment.py
+++ PythonTestQuestions/Assignment.py
@@ -5,39 +5,41 @@ import openpyxl
 
 def create_dataframe():
     file_name = 'Privia Family Medicine 113018.xlsx'
-    data = pd.read_excel(file_name, header=3)
+    data = pd.read_excel(file_name, header=3, usecols='B:M', index_col='ID')
     data = data.dropna(subset=['First Name', 'Sex'])
     new_headers = []
     header = data.columns
     for name in header:
-        new_name = re.sub(r"[^a-zA-Z0-9+\s+$]", '', name)
-        print(new_name)
-        new_name = new_name.upper()
-        new_headers.append(new_name.replace(' ', '_'))
+        name = re.sub('[^A-Za-z0-9]+', ' ', name)
+        new_name = name.upper()
+        new_name = new_name.replace(' ', '_')
+        new_name = new_name.rstrip('_')
+        new_headers.append(new_name)
+    print(new_headers)
     data.columns = new_headers
-
+    data['FILE_DATE'] = (file_name[-12:-5])
     return data, file_name
 
 
 #change the thing so it I used a pivot instead. make the dataframe its own function. Then pass it.
-def import_demographics():
+def upload_demographics():
     data, file_name = create_dataframe()
-    pivot_data = data
-    pivot_data['SEX'] = pivot_data['SEX'].replace({0: 'M', 1: 'F'})
-    pivot_data['MIDDLE_NAME'] = pivot_data['MIDDLE_NAME'].str[0]
-    pivot_data['PROVIDER_GROUP'] = file_name[:-12]
-    pivot_data['FILE_DATE'] = (file_name[-12:-5])
+    data['SEX'] = data['SEX'].replace({0: 'M', 1: 'F'})
+    data['MIDDLE_NAME'] = data['MIDDLE_NAME'].str[0]
+    data['PROVIDER_GROUP'] = file_name[:-12]
+    data = data.drop(['ATTRIBUTED_Q1', 'ATTRIBUTED_Q2', 'RISK_Q1', 'RISK_Q2', 'RISK_INCREASED_FLAG'], axis=1)
+    data.to_sql()
     print(data)
-    pivot_data = pivot_data.pivot(index=None, columns='FILE_DATE' ,values=['ID','FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME' ,'DOB1', 'SEX' ,'FAVORITE_COLOR'])
-    print(pivot_data)
-    return pivot_data
 
-def unpivot():
+def upload_quarterly_risk():
     data, file_name = create_dataframe()
-    return 0
-
+    data.drop(data.columns[[1,2,3,4,5,6,7]], axis=1, inplace=True)
+    indexes = data[data['RISK_INCREASED_FLAG'] == ' No'].index
+    data.drop(indexes, inplace=True)
+    data.drop(data.columns[3], axis=1, inplace=True)
+    print(data)
 
 def main():
-    import_demographics()
-
+    upload_demographics()
+    upload_quarterly_risk()
 main()
