076fd444ed7a4666e4eae2960f284a29df9c1bc9
yahuey <30640620+yahuey@users.noreply.github.com>
2021-12-01 20:12:36 -0700

    Next is to implement the testcases.

--- PythonTestQuestions/Assignment.py
+++ PythonTestQuestions/Assignment.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import re
 from sqlalchemy import create_engine
+import unittest
 import psycopg2
 import openpyxl
 engine = create_engine('postgresql://javierbenitez@localhost:5432/PERSONDATABASE')
+
 
 def create_dataframe():
     file_name = 'Privia Family Medicine 113018.xlsx'
@@ -20,10 +22,12 @@ def create_dataframe():
     print(new_headers)
     data.columns = new_headers
     data['FILE_DATE'] = (file_name[-12:-5])
+    data['FILE_DATE'] = pd.to_datetime(data.FILE_DATE)
+    data['FILE_DATE'] = data['FILE_DATE'].dt.date
     return data, file_name
 
 
-#change the thing so it I used a pivot instead. make the dataframe its own function. Then pass it.
+# change the thing so it I used a pivot instead. make the dataframe its own function. Then pass it.
 def upload_demographics():
     data, file_name = create_dataframe()
     data['SEX'] = data['SEX'].replace({0: 'M', 1: 'F'})
@@ -33,16 +37,33 @@ def upload_demographics():
     data.to_sql('demographics', engine, if_exists='append')
     print(data)
 
+
 def upload_quarterly_risk():
     data, file_name = create_dataframe()
-    data.drop(data.columns[[1,2,3,4,5,6,7]], axis=1, inplace=True)
+    data.drop(data.columns[[1, 2, 3, 4, 5, 6, 7]], axis=1, inplace=True)
     indexes = data[data['RISK_INCREASED_FLAG'] == ' No'].index
     data.drop(indexes, inplace=True)
     data.drop(data.columns[3], axis=1, inplace=True)
-    data.to_sql('quaterly_risk', engine, if_exists='append')
+    data.to_sql('quarterly_risk', engine, if_exists='append')
     print(data)
+
+# Need to create test cases
+
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 5, "Should be 6"
+
 
 def main():
     upload_demographics()
     upload_quarterly_risk()
+    test_sum()
+    test_sum_tuple()
+    print("Everything passed")
+
+
 main()
