1924fc100e75389771b392bd338038dab657a308
yahuey <30640620+yahuey@users.noreply.github.com>
2021-12-02 20:29:11 -0700

    Got the second query to work as expected

--- SQLTestQuestions/TestQuestions.sql
+++ SQLTestQuestions/TestQuestions.sql
@@ -6,7 +6,8 @@ Hello!
 Please use the test data provided in the file 'PersonDatabase' to answer the following
 questions. Please also import the dbo.Contacts flat file to a table for use. 
 
-All answers should be executable on a MS SQL Server 2012 instance. 
+All answers should be executable on a MS SQL Server 2012 instance.
+
 I have no access to run TSQL in my home, and I do not have a microsoft license to virtualize anything. I would have liked
 to follow the standards, it just was not possible currently.
 ***********************
@@ -38,11 +39,10 @@ return the full name and nickname of each person. The nickname should contain on
 or be blank if no nickname exists.
 
 **********************/
-
-SELECT risk.personid,	attributedpayer,	riskscore,	risklevel,	riskdatetime,	personname,
-      		avg(riskscore) OVER(PARTITION BY person.personname, attributedpayer ORDER BY riskdatetime ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS three_day_moving_average
-FROM risk left join person
-on person.personid = risk.personid;
+select trim(leading '(' from (substring(personname, (position('(' in personname)), ((position(')'in personname) - (position('(' in personname))))))) as nickname,
+trim(trailing ')' from replace(personname, concat('(',trim(leading '(' from (substring(personname, (position('(' in personname)), ((position(')'in personname) - (position('(' in personname))))))),')'), '')) as FullName
+from person
+order by FullName, nickname;
 
 /**********************
 
@@ -54,5 +54,9 @@ and a moving average of risk for that patient and payer in dbo.Risk.
 **********************/
 
 
+SELECT risk.personid,	attributedpayer,	riskscore,	risklevel,	riskdatetime,	personname,
+      		avg(riskscore) OVER(PARTITION BY person.personname, attributedpayer ORDER BY riskdatetime ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS three_day_moving_average
+FROM risk left join person
+on person.personid = risk.personid;
 
 
