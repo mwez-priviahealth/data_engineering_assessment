50bb0d3fb364a2cbd941bed05ac632919f87578d
meldj1 <93406129+meldj1@users.noreply.github.com>
2021-12-06 09:06:40 -0500

    Update privia.py

--- PythonTestQuestions/privia.py
+++ PythonTestQuestions/privia.py
@@ -63,7 +63,7 @@ def load():
 	Base = declarative_base()
 
 	if __name__ == "__main__":
-		db_string = "postgresql://postgres:password1@localhost:5432/persondatabase"
+		db_string = "postgresql://postgres:passwor@localhost:5432/persondatabase"
 		engine = create_engine(db_string)
 		Base.metadata.create_all(engine)
 
